d738da1218fc323a76ad05289b8d738b46c1a62b
Narcis Vladutu <Narcis.Vladutu@msg.group>
2022-07-30 16:17:22 +0300

    test commit

--- shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -8,5 +8,6 @@ public class ShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
+        System.out.println("da");
     }
 }
